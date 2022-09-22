08673c33fd09c6dab1020e27f893fced96d57832
Hanna <100651690+Hanna516@users.noreply.github.com>
2022-09-22 16:32:48 +0800

    Add files via upload
    函数

--- test.c
+++ test.c
@@ -2,213 +2,261 @@
 
 #include<stdio.h>
 
-//编写代码模拟用户登录，且只能登三次（只许输三次密码，若密码输入正确则提示登录成功）
-//若三次均输入错误则退出系统
+//计算两个数的值
 
 /*
-* 
-*/
-#include<string.h>
+* int Add(int x, int y) {
+	int z = 0;
+	z = x + y;
+	return z;
+}
 int main()
 {
-	int i = 0;
-	char password[20] = {0};
-	for(i = 0; i < 3; i++)
-	{
-		printf("请输入密码:>");
-		scanf("%s\n", password);
-		if (strcmp(password, "123456")==0)//==不能比较两个数是否相等，应使用库函数strcmp
-		{
-			printf("登陆成功\n");
-			break;
-		}
-		else
-		{
-			printf("密码错误\n");
-		}
-	}
-	if (i == 3)
-	{
-		printf("三次密码均错误，请退出程序\n");
-	}
+	int a = 10;
+	int b = 20;
+	int sum = Add(a, b);
+	printf("%d\n", sum);
 	return 0;
 }
+*/
 
-
-//编写一段代码演示多个字符从两端移动，向中间汇聚
+//库函数strcpy用法-字符串拷贝
+#include<string.h>
 
 /*
-*
-#include<string.h>
-#include<windows.h>
-#include<stdlib.h>
-
-int main()
+* int main()
 {
-	char arr1[] = "welcom to bit!!!!!!";
-	char arr2[] = "###################";
-	int left = 0;
-	//int right = sizeof(arr1) / sizeof(arr1[0]) - 2;//err
-	int right = strlen(arr1)-1;
-	while (left<=right)
-	{
-		arr2[left] = arr1[left];
-		arr2[right] = arr1[right];
-		printf("%s\n", arr2);
-		//休息一秒
-		Sleep(1000);
-		system("cls");//执行系统命令的一个函数-cls-清空系统屏幕
-		left++;
-		right--;
-	}
+	char arr1[] = "bit";
+	char arr2[20] = "##########";
+	strcpy(arr2, arr1);
 	printf("%s\n", arr2);
 	return 0;
 }
-
 */
 
-//写一段代码在有序的arr数组中找到数字7
-
-/*方法一：二分法
-* int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int k = 7;
-	int sz = sizeof(arr) / sizeof(arr[0]);//计算元素个数
-	int left = 0;//左下标
-	int right = sz-1;//右下标
-	while (left<=right)
-	{
-		int mid = (left + right) / 2;
-		if (arr[mid] > k)
-		{
-			right = mid - 1;
-		}
-		else if (arr[mid] < k)
-		{
-			left = mid + 1;
-		}
-		else
-		{
-			printf("找到了，下标是: %d\n", mid);
-			break;
-		}
-	}
-	if (left > right)
-	{
-		printf("找不到\n");
-	}
-	return 0;
-}
-*/
-
-/*方法二     
-* int main()
-{
-	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-	int k = 7;
-	int i = 0; 
-	int sz = sizeof(arr) / sizeof(0);
-	for (i = 0; i < sz; i++)
-	{
-		if (k == arr[i])
-		{
-			printf("找到了，下标是: %d\n", i); 
-			break;
-		}
-	}
-	if (i == sz)
-		printf("找不到\n");
-	return 0;
-}
-*/
-
-
-//求1!+2!+...+10!
-
-/*方法一             
-* int main()
-{
-	int i = 0;
-	int n = 0;
-	int ret = 1;
-	int sum = 0;
-	for (n = 1; n <= 10; n++)
-	{
-		ret = ret * n;
-		sum = sum + ret;
-	}
-	printf("sum = %d", sum);
-	return 0;
-}
-*/
-
-/*方法二
-* int main()
-{
-	int i = 0;
-	int n = 0;
-	int ret = 1;
-	int sum = 0;
-	for (n = 1; n <= 10; n++)
-	{
-		ret=1;
-		for (i= 1; i <= n; i++)
-		{
-			ret = ret * i; 
-		}
-		sum = sum + ret;
-	}
-	printf("sum = %d", sum);
-	return 0;
-}
-*/
-
-
-//求n!
+//库函数 memset-memory 内存  set-设置
 
 /*
 * int main()
 {
+	char arr[] = "hello world";
+	memset(arr, '*', 5);
+	printf("%s\n", arr);
+	//***** world
+	return 0;
+}
+*/
+
+//自定义函数
+
+/*
+* //定义函数
+int get_max(int x, int y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	//函数的使用
+	int max = get_max(a, b);
+	printf("max=%d\n", max);
+	return 0;
+}
+*/
+
+//写一段函数交换两个整形变量内容
+
+/*
+* void Swap1(int x, int y)
+{
+	int tmp = 0;
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+void Swap2(int* pa, int* pb)
+{
+	int tmp = 0;
+	tmp = *pa;
+	*pa = *pb;
+	*pb = tmp;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	//int tmp = 0;
+	//
+	printf("a=%d b=%d\n", a, b);
+	//Swap1(a,b);
+	Swap2(&a,&b);
+	//tmp = a;
+	//a = b;
+	//b = tmp;
+	printf("a=%d b=%d\n", a, b);
+	return 0;
+}
+*/
+
+/*
+* int main()
+{
+	int a = 10;
+	int* pa = &a;//pa指针变量
+	*pa = 20;//解引用操作
+	printf("%d\n", a);
+	return 0;
+}
+*/
+
+#include<math.h> 
+
+//打印100-200间的素数
+
+/*
+* 
+//是素数返回1反之返回0
+int is_prime(int n)
+{
+	//2->n-1
+	int j = 0;
+	for (j = 2; j <= sqrt(n); j++)
+	{
+		if (n % j == 0)
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
 	int i = 0;
-	int n = 0;
-	int ret = 1;
-	scanf("%d", &n);
-	for (i = 1; i <= n; i++)
+	for (i = 100; i <= 200; i++)
 	{
-		ret = ret * i;
+		//判断是否为素数
+		if (is_prime(i) == 1)
+			printf("%d ", i);
 	}
-	printf("ret = %d\n", ret);
 	return 0;
 }
 */
 
+//写一段函数判断是否是闰年
 
-//1-100间的奇数
+/*
+* int is_leap_year(int y)
+{
+	if ((y % 4 == 0 && y % 100 != 0) || (y % 400 == 0))
+		return 1;
+	else
+		return 0;
+}
+int main()
+{
+	int year = 0;
+	for (year = 1000; year <= 2000; year++)
+	{
+		if (1 == is_leap_year(year))
+		{
+			printf("%d ", year);
+		}
+	}
+	return 0;
+}
+*/
 
-/*方法一
+//体现一段整形有序数列的二分查找
+
+/*
+* int binry_search(int arr[],int k,int sz)
+{
+	int left = 0;
+	int right = sz - 1;
+	while(left<=right)
+	{ 
+		int mid = (left + right) / 2;
+		if (arr[mid] < k)
+		{
+			left = mid + 1;
+		}
+		else if (arr[mid] > k)
+		{
+			right = mid - 1;
+		}
+		else
+		{
+			return mid;
+		}
+	}
+	return -1;
+}
+int main()
+{
+	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	int k = 7;
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int ret = binry_search(arr,k,sz);
+	if (ret == -1)
+	{
+		printf("找不到指定的数字\n");
+	}
+	else
+	{
+		printf("找到了,下标是：%d\n", ret); 
+	}
+	return 0;
+}
+*/
+
+//写一个函数每调用一次，num值就加1
+
+/*
+* int Add(int* p)
+{
+	(*p)++;
+}
+int main()
+{
+	int num = 0;
+	Add(&num);
+	printf("num = %d\n", num);//1
+	Add(&num);
+	printf("num = %d\n", num);//2
+	Add(&num);
+	printf("num = %d\n", num);//3
+	return 0;
+}
+*/
+
+//链式访问
+
+/*
 * int main()
 {
-	int i = 1;
-	while (i <= 100)
-	{
-		printf("%d", i);
-		i += 2;
-	}
+	int len = 0;
+	//1
+	len = strlen("abc");
+	printf("%d\n", len);
+	//2
+	printf("%d\n", strlen("abc"));
 	return 0;
 }
 */
 
-/*方法二
-* int main()
-{
-	int i = 1;
-	while (i <= 100)
-	{
-		if (i % 2 == 1)
-			printf(" %d", i);
-		i++; 
-	}
+//函数声明和定义
+
+#include"add.h"
+
+int main() {
+	int a = 10;
+	int b = 20;
+	int sum = 0;
+	//函数调用
+	sum = Add(a, b);
+	printf("%d\n", sum);
 	return 0;
 }
-*/
